9820d8f9e4a38c0a54b9de6429c75694c3733744
dimabalawov <dimabalawov@gmail.com>
2024-10-29 14:19:03 +0200

    Добавьте файлы проекта.

---
+++ DefaultCSharpInterfacesHW/Classes.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    public class Book : ICloneable, IComparable
+    {
+        public string Name { get; private set; } = "NULL";
+        public string Author { get; private set; } = "NULL";
+        public string Genre { get; private set; } = "NULL";
+        public int Year { get; private set; } = -1;
+        public int Pages { get; private set; } = -1;
+
+        public Book(string name, string author, string genre, int year, int pages)
+        {
+            Name = name;
+            Author = author;
+            Genre = genre;
+            Year = year;
+            Pages = pages;
+        }
+
+        public void Show() =>
+            Console.WriteLine($"Name: {Name}\nAuthor: {Author}\nGenre: {Genre}\nYear of Publication: {Year}\nAmount of Pages: {Pages}");
+
+        #region ICloneable
+        public object Clone()
+        {
+            return new Book(Name, Author, Genre, Year, Pages);
+        }
+        #endregion ICloneable
+
+        #region IComparable
+        public int CompareTo(object obj)
+        {
+            if (obj is Book book)
+                return Year.CompareTo(book.Year);
+
+            throw new ArgumentException("Object is not a Book");
+        }
+        #endregion IComparable
+
+        #region IComparer
+        public class SortByName : IComparer
+        {
+            public int Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book book1 && obj2 is Book book2)
+                    return book1.Name.CompareTo(book2.Name);
+
+                throw new ArgumentException("Objects are not of type Book");
+            }
+        }
+
+        public class SortByAuthor : IComparer
+        {
+            public int Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book book1 && obj2 is Book book2)
+                    return book1.Author.CompareTo(book2.Author);
+
+                throw new ArgumentException("Objects are not of type Book");
+            }
+        }
+        public class SortByYear : IComparer
+        {
+            public int Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book book1 && obj2 is Book book2)
+                    return book1.Year.CompareTo(book2.Year);
+
+                throw new ArgumentException("Objects are not of type Book");
+            }
+        }
+        public class SortByGenre : IComparer
+        {
+            public int Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book book1 && obj2 is Book book2)
+                    return book1.Genre.CompareTo(book2.Genre);
+
+                throw new ArgumentException("Objects are not of type Book");
+            }
+        }
+        public class SortByPages : IComparer
+        {
+            public int Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book book1 && obj2 is Book book2)
+                    return book1.Pages.CompareTo(book2.Pages);
+
+                throw new ArgumentException("Objects are not of type Book");
+            }
+        }
+        #endregion IComparer
+    }
+
+    public class Library : IEnumerable, IEnumerator
+    {
+        private Book[] books;
+        private int curpos = -1;
+
+        public Library(Book[] arr)
+        {
+            books = new Book[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+                books[i] = (Book)arr[i].Clone();
+        }
+
+        #region IEnumerator
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public void Reset()
+        {
+            curpos = -1;
+        }
+
+        public object Current => books[curpos];
+
+        public bool MoveNext()
+        {
+            if (curpos < books.Length - 1)
+            {
+                curpos++;
+                return true;
+            }
+            else
+            {
+                Reset();
+                return false;
+            }
+        }
+        #endregion IEnumerator
+    }
+}
---
+++ DefaultCSharpInterfacesHW/Program.cs
@@ -0,0 +1,58 @@
+﻿using Classes;
+
+Book[] bookArray = 
+            {
+                new Book("The Catcher in the Rye", "J.D. Salinger", "Fiction", 1951, 277),
+                new Book("To Kill a Mockingbird", "Harper Lee", "Fiction", 1960, 324),
+                new Book("1984", "George Orwell", "Dystopian", 1949, 328),
+                new Book("The Great Gatsby", "F. Scott Fitzgerald", "Classic", 1925, 180),
+                new Book("Moby-Dick", "Herman Melville", "Adventure", 1851, 635)
+            };
+
+Library library = new Library(bookArray);
+
+Console.WriteLine("Unsorted Books:");
+foreach (Book book in library)
+{
+    book.Show();
+    Console.WriteLine();
+}
+
+Console.WriteLine("Books Sorted by Name:");
+Array.Sort(bookArray, new Book.SortByName());
+foreach (Book book in bookArray)
+{
+    book.Show();
+    Console.WriteLine();
+}
+
+Console.WriteLine("Books Sorted by Author:");
+Array.Sort(bookArray, new Book.SortByAuthor());
+foreach (Book book in bookArray)
+{
+    book.Show();
+    Console.WriteLine();
+}
+Console.WriteLine("Books Sorted by Year:");
+Array.Sort(bookArray, new Book.SortByYear());
+foreach (Book book in bookArray)
+{
+    book.Show();
+    Console.WriteLine();
+}
+
+Console.WriteLine("Books Sorted by Genre:");
+Array.Sort(bookArray, new Book.SortByGenre());
+foreach (Book book in bookArray)
+{
+    book.Show();
+    Console.WriteLine();
+}
+
+Console.WriteLine("Books Sorted by Pages:");
+Array.Sort(bookArray, new Book.SortByPages());
+foreach (Book book in bookArray)
+{
+    book.Show();
+    Console.WriteLine();
+}
